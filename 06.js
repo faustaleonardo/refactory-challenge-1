f57f5fc2232b7a0052a8423f3361763b76728769
Fausta Leonardo <faustaleonardo11@gmail.com>
2020-03-09 16:35:36 +0700

    update 06.js

--- 06.js
+++ 06.js
@@ -12,15 +12,17 @@ let sentence =
   'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 const generateAsterisk = str => {
-  const result = '';
-  for (let i = 0; i < str.length; i++) {
+  let result = '';
+  let arrString = str.split('');
+  for (let i = 0; i < arrString.length; i++) {
     result += '*';
   }
   return result;
 };
 
-for (let word of censoredWords) {
-  sentence = sentence.replace(word, '****');
+for (let wordPattern of censoredWords) {
+  const word = wordPattern.toString().replace(/\//gi, '');
+  sentence = sentence.replace(wordPattern, generateAsterisk(word));
 }
 
 console.log(sentence);
